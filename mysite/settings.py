50563ab7c1d2fba74ad2815c610c0cafe8cff77a
Aikmal <eikmalaqil@gmail.com>
2026-01-05 00:49:46 +0800

    Remediated hardcoded secret key vulnerability

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,3 +1,6 @@
+import os
+from dotenv import load_dotenv
+
 """
 Django settings for mysite project.
 
@@ -14,14 +17,14 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%l9=rrkw!b504)6&chb1ij$gr3@j&#-g$8zdv^e4cqm-=i7hjs'
-
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
